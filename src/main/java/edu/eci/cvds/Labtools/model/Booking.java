bfde1470412cbcfe7e2a8e176303cb90b4f7bfd3
Miguel <miguel.vanegas-c@mail.escuelaing.edu.co>
2025-03-06 16:04:33 -0500

    finish adtabase configuration

--- src/main/java/edu/eci/cvds/Labtools/model/Booking.java
+++ src/main/java/edu/eci/cvds/Labtools/model/Booking.java
@@ -1,12 +1,14 @@
 package edu.eci.cvds.Labtools.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document(collection = "bookings")
 public class Booking {
 
+    @Id
     private String bookingId;
     private int day;
     private int timeLine;
--- src/main/java/edu/eci/cvds/Labtools/model/Lab.java
+++ src/main/java/edu/eci/cvds/Labtools/model/Lab.java
@@ -1,12 +1,13 @@
 package edu.eci.cvds.Labtools.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document(collection = "laboratories")
 public class Lab {
-
+    @Id
     private String labId;
     private boolean[][] isAvailable;
 }
