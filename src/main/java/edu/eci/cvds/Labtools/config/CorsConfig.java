7adf07fade60383ba3740e09174c9e1c69bb437d
miguelvanegas-c <miguel.vanegas-c@mail.escuelaing.edu.co>
2025-04-01 22:34:20 -0500

    Update CorsConfig.java

--- src/main/java/edu/eci/cvds/Labtools/config/CorsConfig.java
+++ src/main/java/edu/eci/cvds/Labtools/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000","skywebnet-bfehbsd7eedzd6fq.eastus-01.azurewebsites.net")
+                        .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
