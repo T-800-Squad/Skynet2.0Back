109a051101ecf385cb9225f3f501bf75906caa0a
Miguel <miguel.vanegas-c@mail.escuelaing.edu.co>
2025-04-01 22:17:06 -0500

    finish back

--- src/main/java/edu/eci/cvds/Labtools/config/JwtFilter.java
+++ src/main/java/edu/eci/cvds/Labtools/config/JwtFilter.java
@@ -24,7 +24,8 @@ public class JwtFilter extends OncePerRequestFilter {
             throws ServletException, IOException {
         String path = request.getRequestURI();
         String header = request.getHeader("Authorization");
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Origin", "\n" +
+                "skywebnet-bfehbsd7eedzd6fq.eastus-01.azurewebsites.net");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS,DELETE");
         response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
 
